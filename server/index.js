4e3e7711b800bfa4bf10b5ad7704bfd3aa40786c
sweetbacon <nguyentrungsb1005@gmail.com>
2024-02-06 10:07:51 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 const db = require('./configs/db');
 const app = express();
 const port = 3000;
 const route = require('./routes');
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'))
 db.connect();
 route(app);
 app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ server/middlewares/ErrorHandler.js
@@ -0,0 +1,19 @@
+const errorHandler = (err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid token',
+        })
+    }
+    if (err.name === 'ValidationError') {
+        return res.status(400).json({
+            success: false,
+            message: err.message,
+        })
+    }
+    return res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+        error: err.message,
+    })
+}
