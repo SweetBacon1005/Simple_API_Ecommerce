2ba82a86181a4134c0d5b2cfaa9bb3d202d34384
sweetbacon <nguyentrungsb1005@gmail.com>
2024-04-05 15:13:39 +0700

    'update'

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@ const ProductRoutes = require("./ProductRoute");
 const CategoryRoutes = require("./CategoryRoute");
 const CartItemRoutes = require("./CartItemRoute");
 const OrderRoutes = require("./OrderRoute");
+const UserRoutes = require("./UserRoute");
 function route(app) {
   app.use("/auth", AuthRoutes);
   app.use("/category", CategoryRoutes);
   app.use("/product", ProductRoutes);
   app.use("/cart-item", CartItemRoutes);
   app.use("/order", OrderRoutes);
+  app.use("/user", UserRoutes);
 }
 module.exports = route;
